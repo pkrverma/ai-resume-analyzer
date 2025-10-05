d6bfe7a1263fdb7478f915ddccc19f25a55b3f95
Pulkit verma <pulkit18012003@gmail.com>
2025-10-05 17:17:53 +0530

    added feedback button in homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -131,9 +131,9 @@ export default function Home() {
       </section>
 
       {/* Feedback Button */}
-      <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-6 right-6 sm:bottom-5 sm:right-5 md:bottom-6 md:right-6 z-50">
         <button
-          onClick={() => window.open('https://forms.gle/YOUR_GOOGLE_FORM_ID', '_blank')}
+          onClick={() => window.open('https://forms.gle/ysnTnNQFyvs7QkRc8', '_blank')}
           onMouseEnter={() => hasInitialAnimationCompleted && setIsHovered(true)}
           onMouseLeave={() => hasInitialAnimationCompleted && setIsHovered(false)}
           className={`feedback-button ${
@@ -142,7 +142,7 @@ export default function Home() {
           title="Share your feedback"
         >
           <div className="feedback-icon">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H5.17L4 17.17V4H20V16Z"
                 fill="currentColor"
